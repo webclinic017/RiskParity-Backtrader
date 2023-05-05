4fc2d1d1a81755a300321be2e56e1fa700a7152b
ra6it <kitmurraypyle@gmail.com>
2023-05-06 09:30:41 +1200

    debug

--- Strategy_BackTest/datamanagement.py
+++ Strategy_BackTest/datamanagement.py
@@ -10,6 +10,7 @@ def data_management(start, end, int):
     holdings_url = "https://github.com/ra6it/RiskParity/blob/main/RiskParity_Holdings_Constraints.xlsx?raw=true"
     holdings_url = requests.get(holdings_url, verify=False).content
     assets = pd.read_excel(holdings_url,'Holdings',usecols="A:B", engine='openpyxl')
+    print(assets)
     assets = assets.reindex(columns=['Asset', 'Industry', 'Full_name'])
     asset_classes = {'Asset': assets['Asset'].values.tolist(), 
                      'Industry': assets['Industry'].values.tolist(),
@@ -18,6 +19,7 @@ def data_management(start, end, int):
     asset_classes = pd.DataFrame(asset_classes)
     asset_classes = asset_classes.sort_values(by=['Asset'])
     asset = assets['Asset'].values.tolist()
+    print(asset)
     #asset = list(set(asset))
     asset = [x for x in asset if str(x) != 'nan']
 
